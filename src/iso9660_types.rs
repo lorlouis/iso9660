973750d9b999b7db5d2fe46eeacbc360744e7726
lorlouis <louis.sven@gmail.com>
2023-05-22 13:26:50 -0400

    added a bunch of types

---
+++ src/iso9660_types.rs
@@ -0,0 +1,288 @@
+use std::io::{self, Read};
+use std::ops::RangeInclusive;
+
+const STR_A_CHAR_SET: &[u8] = concat!(
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "0123456789_",
+    "!\"%&'()*+,-./:;<=>?")
+    .as_bytes();
+
+const STR_A_CHAR_SET_BIT_SET: [u8; 16] = build_ascii_bit_set(STR_A_CHAR_SET);
+
+const fn build_ascii_bit_set(alphabet: &[u8]) -> [u8; 16] {
+    let mut bitset = [0_u8; 16];
+    let mut index = 0;
+    let len = alphabet.len();
+    while index < len {
+        let b = alphabet[index];
+        let byte_index = b / 8;
+        let bit_index = (b & 7).saturating_sub(1);
+        bitset[byte_index as usize] |= 1 << bit_index;
+        index += 1;
+    }
+    bitset
+}
+
+pub struct StrA<const LEN: usize> {
+    bytes: [u8; LEN],
+    padding: usize,
+}
+
+pub struct InvalidChar(u8);
+
+impl<const LEN: usize> StrA<LEN> {
+
+    pub fn as_str(&self) -> &str {
+        unsafe {
+            std::str::from_utf8_unchecked(&self.bytes[self.padding..])
+        }
+    }
+
+    /// SAFETY: `slice` must be of size LEN
+    pub fn from_slice(slice: &[u8]) -> Result<Self, InvalidChar> {
+        assert_eq!(slice.len(), LEN, "`slice` must be of size LEN");
+        for b in slice {
+            let byte_index = b / 8;
+            let bit_index = (b & 7).saturating_sub(0);
+            let bit_mask = 1 << bit_index;
+            if bit_mask & STR_A_CHAR_SET_BIT_SET[byte_index as usize] == 0 {
+                return Err(InvalidChar(*b))
+            }
+        }
+        unsafe {
+            Ok(Self::from_slice_unchecked(slice))
+        }
+    }
+
+    /// # Safety `slice` must be of size LEN and the char set must respect `STR_A_CHAR_SET`
+    pub unsafe fn from_slice_unchecked(slice: &[u8]) -> Self {
+        let padding = slice.iter().take_while(|b| **b == b' ').count();
+        let mut bytes = [0_u8; LEN];
+        bytes.copy_from_slice(slice);
+        Self {
+            bytes,
+            padding,
+        }
+    }
+}
+
+
+const STR_D_CHAR_SET: &[u8] = concat!(
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "0123456789_").as_bytes();
+
+const STR_D_CHAR_SET_BIT_SET: [u8; 16] = build_ascii_bit_set(STR_A_CHAR_SET);
+
+pub struct StrD<const LEN: usize> {
+    bytes: [u8; LEN],
+    padding: usize,
+}
+
+impl<const LEN: usize> StrD<LEN> {
+
+    pub fn as_str(&self) -> &str {
+        unsafe {
+            std::str::from_utf8_unchecked(&self.bytes[self.padding..])
+        }
+    }
+
+    /// SAFETY: `slice` must be of size LEN
+    pub fn from_slice(slice: &[u8]) -> Result<Self, InvalidChar> {
+        assert_eq!(slice.len(), LEN, "`slice` must be of size LEN");
+        for b in slice {
+            let byte_index = b / 8;
+            let bit_index = (b & 7).saturating_sub(0);
+            let bit_mask = 1 << bit_index;
+            if bit_mask & STR_A_CHAR_SET_BIT_SET[byte_index as usize] == 0 {
+                return Err(InvalidChar(*b))
+            }
+        }
+        unsafe {
+            Ok(Self::from_slice_unchecked(slice))
+        }
+    }
+    /// # Safety `slice` must be of size LEN and the char set must respect `STR_D_CHAR_SET`
+    pub unsafe fn from_slice_unchecked(slice: &[u8]) -> Self {
+        let padding = slice.iter().take_while(|b| **b == b' ').count();
+        let mut bytes = [0_u8; LEN];
+        bytes.copy_from_slice(slice);
+        Self {
+            bytes,
+            padding,
+        }
+    }
+}
+
+pub struct DecDateTime {
+    year: StrD<4>,
+    month: StrD<2>,
+    day: StrD<2>,
+    hour: StrD<2>,
+    minute: StrD<2>,
+    second: StrD<2>,
+    centi_sec: StrD<2>,
+    /// Time zone offset from GMT in 15 minute intervals, starting at interval -48 (0)
+    time_zone: u8,
+}
+
+
+
+pub enum DecDateTimeErr {
+    Io(io::Error),
+    InvalidChar(u8),
+    InvalidDate {
+        range: RangeInclusive<&'static str>,
+        actual: String,
+    },
+}
+
+impl From<io::Error> for DecDateTimeErr {
+    fn from(value: io::Error) -> Self {
+        DecDateTimeErr::Io(value)
+    }
+}
+
+impl From<InvalidChar> for DecDateTimeErr {
+    fn from(value: InvalidChar) -> Self {
+        Self::InvalidChar(value.0)
+    }
+}
+
+impl DecDateTime {
+    pub fn try_parse<R: Read>(mut r: R) -> Result<Self, DecDateTimeErr> {
+        let mut buffer = [0_u8; 17];
+        r.read_exact(&mut buffer)?;
+
+        let year = StrD::<4>::from_slice(&buffer[..4])?;
+        if !("1".."9999").contains(&year.as_str()) {
+            return Err(DecDateTimeErr::InvalidDate {
+                range: "1"..="9999",
+                actual: year.as_str().to_string(),
+            })
+        }
+
+        let month = StrD::<2>::from_slice(&buffer[4..6])?;
+        if !("1".."9999").contains(&year.as_str()) {
+            return Err(DecDateTimeErr::InvalidDate {
+                range: "1"..="12",
+                actual: month.as_str().to_string(),
+            })
+        }
+
+        let day = StrD::<2>::from_slice(&buffer[6..8])?;
+        if !("1".."31").contains(&year.as_str()) {
+            return Err(DecDateTimeErr::InvalidDate {
+                range: "1"..="31",
+                actual: day.as_str().to_string(),
+            })
+        }
+
+        let hour = StrD::<2>::from_slice(&buffer[8..10])?;
+        if !("0".."23").contains(&year.as_str()) {
+            return Err(DecDateTimeErr::InvalidDate {
+                range: "1"..="23",
+                actual: hour.as_str().to_string(),
+            })
+        }
+
+        let minute = StrD::<2>::from_slice(&buffer[10..12])?;
+        if !("0".."59").contains(&year.as_str()) {
+            return Err(DecDateTimeErr::InvalidDate {
+                range: "0"..="59",
+                actual: minute.as_str().to_string(),
+            })
+        }
+
+        let second = StrD::<2>::from_slice(&buffer[12..14])?;
+        if !("0".."59").contains(&year.as_str()) {
+            return Err(DecDateTimeErr::InvalidDate {
+                range: "0"..="59",
+                actual: second.as_str().to_string(),
+            })
+        }
+
+        let centi_sec = StrD::<2>::from_slice(&buffer[14..16])?;
+        if !("0".."99").contains(&year.as_str()) {
+            return Err(DecDateTimeErr::InvalidDate {
+                range: "0"..="99",
+                actual: centi_sec.as_str().to_string(),
+            })
+        }
+
+        let time_zone = buffer[16];
+        Ok(Self {
+            year,
+            month,
+            day,
+            hour,
+            minute,
+            second,
+            centi_sec,
+            time_zone,
+        })
+    }
+}
+
+pub struct DoubleEndianI16 {
+    pub le: i16,
+    pub be: i16,
+}
+
+impl DoubleEndianI16 {
+    pub fn native_endian(&self) -> i16 {
+        if cfg!(target_endian = "big") {
+            self.be
+        }
+        else {
+            self.le
+        }
+    }
+}
+
+pub struct DoubleEndianU16 {
+    pub le: u16,
+    pub be: u16,
+}
+
+impl DoubleEndianU16 {
+    pub fn native_endian(&self) -> u16 {
+        if cfg!(target_endian = "big") {
+            self.be
+        }
+        else {
+            self.le
+        }
+    }
+}
+
+pub struct DoubleEndianU32 {
+    pub le: u32,
+    pub be: u32,
+}
+
+impl DoubleEndianU32 {
+    pub fn native_endian(&self) -> u32 {
+        if cfg!(target_endian = "big") {
+            self.be
+        }
+        else {
+            self.le
+        }
+    }
+}
+
+pub struct DoubleEndianI32 {
+    pub le: i32,
+    pub be: i32,
+}
+
+impl DoubleEndianI32 {
+    pub fn native_endian(&self) -> i32 {
+        if cfg!(target_endian = "big") {
+            self.be
+        }
+        else {
+            self.le
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-use std::io::Read;
+use std::io::{self, Read};
+use std::iter::repeat;
+use std::ops::RangeInclusive;
+
+mod iso9660_types;
+use iso9660_types::*;
 
 const SECTOR_SIZE: usize = 2 * 1024; // 2K
 
@@ -15,13 +20,112 @@ pub enum VDType {
     VDEnd = 255,
 }
 
-struct VD {
+pub struct UnknownVersion(pub u8);
+
+impl TryFrom<u8> for VDType {
+    type Error=UnknownVersion;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::BootRecord),
+            1 => Ok(Self::PrimaryVD),
+            2 => Ok(Self::EVD),
+            3 => Ok(Self::PartDes),
+            255 => Ok(Self::VDEnd),
+            _ => Err(UnknownVersion(value)),
+        }
+    }
+}
+
+pub struct VD {
     ty: VDType,
     version: u8,
 }
 
-impl VD {
+pub enum VDErr {
+    Io(io::Error),
+    UnknownVersion(u8),
+    UnknownIdent([u8;5]),
+    InvalidAlphabet(u8, &'static [u8]),
+    InvalidDate {
+        range: RangeInclusive<&'static str>,
+        actual: String,
+    },
+}
 
+impl From<io::Error> for VDErr {
+    fn from(value: io::Error) -> Self {
+        Self::Io(value)
+    }
+}
+
+impl From<UnknownVersion> for VDErr {
+    fn from(value: UnknownVersion) -> Self {
+        Self::UnknownVersion(value.0)
+    }
+}
+
+impl VD {
+    pub fn read_header<R: Read>(mut r: R) -> Result<Self, VDErr> {
+        let mut buffer = [0_u8; 7];
+        r.read_exact(&mut buffer)?;
+
+        let ty = VDType::try_from(buffer[0])?;
+
+        let mut ident = [0_u8; 5];
+        ident.clone_from_slice(&buffer[1..6]);
+        if &ident != VD_IDENT {
+            return Err(VDErr::UnknownIdent(ident))
+        }
+
+        let version = buffer[6];
+        if version != 1 {
+            return Err(VDErr::UnknownVersion(version));
+        }
+
+        Ok(Self {
+            ty,
+            version,
+        })
+    }
+}
+
+pub struct PVD {
+    sys_ident: StrA<32>,
+    vol_ident: StrD<32>,
+    vol_space_size: u32,
+    vol_set_size: u16,
+    vol_seq_num: u16,
+    logical_block_size: u16,
+    path_table_size: u32,
+    path_table_l_location: u32,
+    opt_path_table_location: Option<u32>,
+    vol_set_ident: StrD<128>,
+    publisher_ident: StrA<128>,
+    data_prep_ident: StrA<128>,
+    app_ident: StrA<128>,
+    copyright_file_name: Option<StrD<37>>,
+    abstract_file_name: Option<StrD<37>>,
+    bibliographic_file_name: Option<StrD<37>>,
+    vol_create_date_time: DecDateTime,
+    vol_mod_date_time: DecDateTime,
+    vol_expiration_date_time: DecDateTime,
+    vol_effective_date_time: DecDateTime,
+    application_used: StrA<512>,
+}
+
+impl PVD {
+    pub fn try_parse<R: Read>(mut r: R) -> Result<Self, VDErr> {
+        // TODO(louis) read 2k and interpret it
+
+        // substitute the size of the header
+        let sector_minus_header = SECTOR_SIZE-6;
+        // try not to allocate 2k in the stack
+        let mut buffer: Vec<u8> = repeat(0_u8).take(sector_minus_header).collect();
+        r.read_exact(&mut buffer)?;
+
+        todo!()
+    }
 }
 
 #[cfg(test)]
